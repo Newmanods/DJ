f2d3bb5e73b63e4e1209fd4749a7e83541857017
Samuel Newman <125279733+Newmanods@users.noreply.github.com>
2023-07-27 12:00:58 +0000

    Add files via upload

---
+++ DJ Lord/JS/app.js
@@ -0,0 +1,107 @@
+// let ratings = 6;
+
+// if (ratings === 3){
+//     console.log("it's Working")
+// }
+// else if (ratings <= 2){
+//     console.log("Next time bro")
+// }
+// else if(ratings >=4 && ratings<5){
+//     console.log("MEETS EXPECTATIONS")
+// }
+// else{
+//     console.log('Invalid ratings')
+// }
+
+// let highScore = 100;
+// let userScore = 78;
+
+// if(userScore >= highScore){
+//     console.log(`Congrat's You've a New High Score ${userScore}`);
+//     // highScore = userScore;
+// }
+// else{
+//     console.log(`Your score didn't beat the High score ${userScore}`)
+// }
+
+
+
+// let password = "HelloNewman";
+
+// if (password.length >=6){
+//      if(password.indexOf(' ')=== -1){
+//         console.log('Valid Pasword');
+//      }
+//      else{
+//         console.log('Password is long enough,but cannot contain space');
+//      }
+        
+// }
+// else{
+//     console.log('password must be longer');
+// }
+
+// let number = 5;
+
+// if (number){
+//     console.log('TRUTHY')
+// }
+// else{
+//     console.log("falsy")
+// }
+
+// Logical Operator AND (&&)
+
+// let num = 1;
+
+// if(num >= 1 && num <= 10){
+//     console.log('Number is between 1 and 10');
+// }
+// else{
+//     console.log('Pleas guess a number between 1 and 10');
+// }
+
+// // Logical Operator OR (||)
+// let age = 76;
+
+//  if(age < 6 || age >= 65){
+//     console.log('You get in for free!')
+//  }
+// else{
+//     console.log('That will be $10 please')
+// } 
+
+// // Switch Statement
+// let day =6;
+ 
+// switch (day){
+//     case 1:
+//         console.log('Monday');
+//         break;
+//     case 2:
+//         console.log('Tuesday');
+//         break;
+//     case 3:
+//         console.log('Wednesday');
+//         break;      
+//     case 4:
+//         console.log('Thursday');
+//         break;
+//     case 5:
+//         console.log('Friday');
+//         break;
+//     case 6:
+//         console.log('Saturday');
+//         break; 
+//     case 7:
+//         console.log('Sunday')                     
+// }
+//   const fitBitData = {
+//     totalSteps: 3000,
+//     totalMiles: 211.7,
+//     avgCaloriBurn:'5 of 7',
+//     AvgGoodSleep: '2.13'
+//   } 
+   const userReview = {};
+
+   userReview ['queenBee19'] = 4.3;
